3f8e365708c666904ec939f3e699c3c97ad8d120
anurag-bit <singh.ask@outlook.com>
2022-03-26 11:16:41 +0000

    my nem

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <Container>
       <div className='flex flex-col items-center justify-center py-2'>
-        <BigText slides={['NextJs.', 'TailwindCSS.', 'Typescript.']} />
+        <BigText slides={['Anurag', 'Kumar', 'Singh']} />
       </div>
     </Container>
   )
